b2621c79760585b581454f0bfc610086ab463db3
hvieira <scythe999@gmail.com>
2019-07-19 11:14:27 +0100

    Add exercise 2.2 + test refactor to use wordspec

--- manning-functional-programming-scala/src/main/scala/chapter2/Chapter2.scala
+++ manning-functional-programming-scala/src/main/scala/chapter2/Chapter2.scala
@@ -4,6 +4,7 @@ object Chapter2 {
 
   def fib(n: Int): Int = {
 
+    @annotation.tailrec
     def inner(desired: Int, curr: Int, prev1: Int, prev2: Int): Int = desired match {
       case x if x == curr => prev1 + prev2
       case _ => inner(desired, curr + 1, prev2, prev1 + prev2)
@@ -14,6 +15,18 @@ object Chapter2 {
       case 1 => 1
       case _ => inner(n, curr = 2, prev1 = 0, prev2 = 1)
     }
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+
+    def loop(i: Int): Boolean = {
+      if (as.length - i >= 2)
+        if (ordered(as(i), as(i+1))) loop(i+1) else false
+      else
+        true
+    }
+
+    if  (as.isEmpty) true else loop(0)
   }
 
 }
--- manning-functional-programming-scala/src/test/scala/chapter2/Chapter2Spec.scala
+++ manning-functional-programming-scala/src/test/scala/chapter2/Chapter2Spec.scala
@@ -1,43 +1,91 @@
 package chapter2
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 
-class Chapter2Spec extends FlatSpec with Matchers {
+class Chapter2Spec extends WordSpec with Matchers {
 
-  "Fib of 0" should "be 0" in {
-    Chapter2.fib(0) shouldEqual 0
+  "fib" must {
+    "be 0 for n 0" in {
+      Chapter2.fib(0) shouldEqual 0
+    }
+
+    "be 1 for n 1" in {
+      Chapter2.fib(1) shouldEqual 1
+    }
+
+    "be 1 for n 2" in {
+      Chapter2.fib(2) shouldEqual 1
+    }
+
+    "be 2 for n 3" in {
+      Chapter2.fib(3) shouldEqual 2
+    }
+
+    "be 3 for n 4" in {
+      Chapter2.fib(4) shouldEqual 3
+    }
+
+    "be 5 for n 5" in {
+      Chapter2.fib(5) shouldEqual 5
+    }
+
+    "be 8 for n 6" in {
+      Chapter2.fib(6) shouldEqual 8
+    }
+
+    "be 13 for n 7" in {
+      Chapter2.fib(7) shouldEqual 13
+    }
+
+    "be 233 for n 13" in {
+      Chapter2.fib(13) shouldEqual 233
+    }
+
   }
 
-  "Fib of 1" should "be 1" in {
-    Chapter2.fib(1) shouldEqual 1
-  }
+  "isSorted" must {
 
-  "Fib of 2" should "be 1" in {
-    Chapter2.fib(2) shouldEqual 1
-  }
+    val intAscOrderPredicate: (Int, Int) => Boolean = (a: Int, b: Int) => a < b
+    val intDescOrderPredicate: (Int, Int) => Boolean = (a: Int, b: Int) => a > b
 
-  "Fib of 3" should "be 2" in {
-    Chapter2.fib(3) shouldEqual 2
-  }
+    "say an array is sorted if empty" in {
+      Chapter2.isSorted(Array.empty[Int], intAscOrderPredicate) should be(true)
+      Chapter2.isSorted(Array.empty[Int], intDescOrderPredicate) should be(true)
+    }
 
-  "Fib of 4" should "be 3" in {
-    Chapter2.fib(4) shouldEqual 3
-  }
+    "say an array is sorted if has a single value" in {
+      Chapter2.isSorted(Array(1), intAscOrderPredicate) should be(true)
+      Chapter2.isSorted(Array(456812465), intAscOrderPredicate) should be(true)
 
-  "Fib of 5" should "be 5" in {
-    Chapter2.fib(5) shouldEqual 5
-  }
+      Chapter2.isSorted(Array(1), intDescOrderPredicate) should be(true)
+      Chapter2.isSorted(Array(456812465), intDescOrderPredicate) should be(true)
+    }
 
-  "Fib of 6" should "be 8" in {
-    Chapter2.fib(6) shouldEqual 8
-  }
+    "say an array is sorted if sorted as expected by the provided function" in {
+      Chapter2.isSorted(Array(1, 2), intAscOrderPredicate) should be(true)
+      Chapter2.isSorted(Array(1, 2, 3), intAscOrderPredicate) should be(true)
+      Chapter2.isSorted(Array(3, 84795), intAscOrderPredicate) should be(true)
 
-  "Fib of 7" should "be 13" in {
-    Chapter2.fib(7) shouldEqual 13
-  }
+      Chapter2.isSorted(Array(2, 1), intDescOrderPredicate) should be(true)
+      Chapter2.isSorted(Array(3, 2, 1), intDescOrderPredicate) should be(true)
+      Chapter2.isSorted(Array(84795, 3), intDescOrderPredicate) should be(true)
+    }
 
-  "Fib of 13" should "be 233" in {
-    Chapter2.fib(13) shouldEqual 233
+    "say an array is NOT sorted if NOT sorted as expected by the provided function" in {
+      Chapter2.isSorted(Array(3, 1), intAscOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(3, 2, 1), intAscOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(3, 1, 2), intAscOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(2, 1, 3), intAscOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(2, 3, 1), intAscOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(84795, 3), intAscOrderPredicate) should be(false)
+
+      Chapter2.isSorted(Array(1, 3), intDescOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(1, 2, 3), intDescOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(3, 1, 2), intDescOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(2, 1, 3), intDescOrderPredicate) should be(false)
+      Chapter2.isSorted(Array(3, 84795), intDescOrderPredicate) should be(false)
+    }
+
   }
 
 }
